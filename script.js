5b751cb85ce3f6a4bb9db3e6a8e568cb5f58b887
Prem Sidapara <pprem1644@gmail.com>
2025-04-22 13:36:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let startTime = 0;
+let endTime = 0;
+let timerInterval;
+let isRunning = false;
+
+const display = document.querySelector('.display');
+const startBtn = document.getElementById('startBtn');
+const pasuseBtn = document.getElementById('pauseBtn');
+const Resetbtn = document.getElementById('Resetbtn');
+const lap = document.getElementById('lap');
+const laplist = document.getElementById('laplist');
+
